8971e3918c52556b3292a79270584f44b4c6f33d
George Mendoza <gsmendoza@gmail.com>
2022-09-21 16:19:59 +0800

    Remove duplicate routes block

--- config/routes.rb
+++ config/routes.rb
@@ -70,16 +70,3 @@ Rails.application.routes.draw do
   get '/unauthorized', to: 'home#unauthorized', as: :unauthorized
   get '/cart_link', to: 'store#cart_link', as: :cart_link
 end
-Rails.application.routes.draw do
-  # This line mounts Solidus's routes at the root of your application.
-  # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
-  # If you would like to change where this engine is mounted, simply change the :at option to something different.
-  #
-  # We ask that you don't use the :as option here, as Solidus relies on it being the default of "spree"
-  mount Spree::Core::Engine, at: '/'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
