3e9b162e9237efcf661d9d6f1f3d9f68bd00fde8
George Mendoza <gsmendoza@gmail.com>
2022-12-08 18:25:09 +0800

    Try setting table_name_prefix to solidus_paypal_braintree

--- config/initializers/solidus_braintree.rb
+++ config/initializers/solidus_braintree.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+module SolidusBraintree
+   def self.table_name_prefix
+     'solidus_paypal_braintree_'
+   end
+end
+
 SolidusBraintree.configure do |config|
   # TODO: Remember to change this with the actual preferences you have implemented!
   # config.sample_preference = 'sample_value'
