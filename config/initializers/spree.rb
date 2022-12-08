4b08b4d7fa4f649a1ed1902906362ac5fe751f74
George Mendoza <gsmendoza@gmail.com>
2022-12-08 17:54:30 +0800

    Add braintree_credentials as static model preferences

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -80,3 +80,17 @@ end
 # the class name:
 #
 # Spree::UserLastUrlStorer.rules << 'Spree::UserLastUrlStorer::Rules::AuthenticationRule'
+
+Rails.application.config.to_prepare do
+  Spree::Config.static_model_preferences.add(
+    SolidusBraintree::Gateway,
+    'braintree_credentials', {
+      environment: Rails.env.production? ? 'production' : 'sandbox',
+      merchant_id: ENV['BRAINTREE_MERCHANT_ID'],
+      public_key: ENV['BRAINTREE_PUBLIC_KEY'],
+      private_key: ENV['BRAINTREE_PRIVATE_KEY'],
+      paypal_flow: 'vault', # 'checkout' is accepted too
+      use_data_collector: true # Fingerprint the user's browser when using Paypal
+    }
+  )
+end
