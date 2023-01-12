27cdd5142024033180966b499cd68dd44c4a9607
George Mendoza <gsmendoza@gmail.com>
2023-01-12 18:18:32 +0800

    Copy SolidusBraintree app code
    Exlude ApplicationRecord.

---
+++ app/assets/javascripts/spree/backend/solidus_paypal_braintree.js
@@ -0,0 +1,2 @@
+// @deprecated since 2.0.0. Require spree/backend/solidus_braintree instead.
+//= require spree/backend/solidus_braintree
---
+++ app/assets/javascripts/spree/frontend/solidus_paypal_braintree.js
@@ -0,0 +1,2 @@
+// @deprecated since 2.0.0. Require spree/frontend/solidus_braintree instead.
+//= require spree/frontend/solidus_braintree
