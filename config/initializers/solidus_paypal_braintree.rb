8be03ef0997bba718ff369497a5c5ad9bcc65208
George Mendoza <gsmendoza@gmail.com>
2023-01-31 16:36:03 +0800

    Test another reference to SolidusPaypalBraintree

--- config/initializers/solidus_paypal_braintree.rb
+++ config/initializers/solidus_paypal_braintree.rb
@@ -4,3 +4,5 @@ SolidusPaypalBraintree.configure do |config|
   # TODO: Remember to change this with the actual preferences you have implemented!
   # config.sample_preference = 'sample_value'
 end
+
+SolidusPaypalBraintree # Testing another reference to SolidusPaypalBraintree
