6d5421195d9fc3ee8daf9dad9678b5c8931a3762
George Mendoza <gsmendoza@gmail.com>
2023-02-06 11:05:19 +0800

    Fix paypal spec

--- spec/solidus_braintree_helper.rb
+++ spec/solidus_braintree_helper.rb
@@ -2,3 +2,5 @@ require 'solidus_starter_frontend_helper'
 
 require 'support/solidus_braintree/capybara'
 require 'support/solidus_braintree/gateway_helpers'
+
+ApplicationController.prepend_view_path "spec/fixtures/views"
